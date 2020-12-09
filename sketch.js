473938580eca8cd26295b8d4d07550a4c7124c7b
Danikakhanna <72741042+Danikakhanna@users.noreply.github.com>
2020-12-09 08:42:58 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,24 @@
-var mobj,fobj
+//declaring alias to avoid use of big names for matter modules
+ const Engine=Matter.Engine;
+  const World= Matter.World;
+  const Bodies=Matter.Bodies; 
+  //declaring variables to create game world and engine using matter modules
+   var myEngine,myWorld,ground,box1,box2,box3,box4,ball;
 function setup() {
-  createCanvas(800,400);
-  mobj=createSprite(600, 200, 100, 50);
-  fobj=createSprite(400, 200, 50, 100);
-  mobj.shapeColor="green";
-fobj.shapeColor="green";
+   //create canvas 
+   createCanvas(600,400);
+    //create game engine using create method of Engine Module of Matter
+     myEngine=Engine.create();
+      //myWorld is the myEngine's world
+       myWorld=myEngine.world;
+       ground=new Ground()
+       box1=new Box(100,100,20,20);
+       box2=new Box(100,50,20,40);
 }
-
-function draw() {
-  background(255,255,255);  
-  mobj.x=mouseX;
-  mobj.y=mouseY;
-  console.log(fobj.width/2+mobj.width/2);
-  console.log(mobj.x-fobj.x);
-  console.log(fobj.x-mobj.x);
-if (mobj.x-fobj.x<fobj.width/2+mobj.width/2 &&
-  mobj.x-fobj.x<fobj.width/2+mobj.width/2 && 
-  mobj.y-fobj.y<fobj.width/2+mobj.width/2 &&
-  mobj.y-fobj.y<fobj.width/2+mobj.width/2) {
-    mobj.shapeColor="yellow";
-    fobj.shapeColor="yellow";
-}
- else{
-  mobj.shapeColor="green";
-  fobj.shapeColor="green";
- }
-drawSprites();
+function draw(){
+  background(0);
+  Engine.update(myEngine)
+  ground.display();
+  box1.display();
+  box2.display();
 }
